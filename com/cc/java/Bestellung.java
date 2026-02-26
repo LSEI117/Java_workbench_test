f98f12e0c927a10991badec02b7b13cc9bc60fa9
LSEI117 <l.seid@gmx.de>
2026-02-26 11:09:48 +0000

    03 / Iteration

---
+++ com/cc/java/Bestellung.java
@@ -0,0 +1,48 @@
+package com.cc.java;
+
+public class Bestellung {
+
+
+public static void main(String[] args) {
+ 
+    // Variablen deklarieren
+        String food = "noch kein Burger gewählt";
+        String drink = "noch kein Getränk gewählt";
+        String itemNumStr;
+        String flag;
+
+        System.out.println(food);
+        System.out.println(drink);
+
+         // Bestelldialog 1 : Ausgabe
+        System.out.println("-------------------------------");
+        System.out.println("Herzlich Willkommen bei MD, Ihre Bestellung bitte!");
+        System.out.println("Hamburger(1), Cheeseburger(2) oder Chilliburger(3) ?");
+
+        // Bestelldialog 1 : Eingabe
+        itemNumStr = System.console().readLine("Ihre Wahl? : ");
+        // System.out.println(itemNumStr);  // 1 oder 2 oder 3
+
+         // Zuordnung Zahl --> Burger
+        switch (itemNumStr)
+        {
+            case "1":
+                food = "Hamburger";
+                break;
+            case "2":
+                food = "Cheeseburger";
+                break;
+            case "3":
+                food = "Chilliburger";
+                break;
+        }
+
+         System.out.println(food);
+
+
+    
+
+      
+    }
+
+}
